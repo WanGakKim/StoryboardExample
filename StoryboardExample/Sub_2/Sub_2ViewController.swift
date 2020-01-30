124597eaf4c11b93bb08695ee2177da70af3b768
wangak kim <wangak.kim@holich.net>
2020-01-31 02:11:42 +0900

    Add Sub_2ViewController

---
+++ StoryboardExample/Sub_2/Sub_2ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Sub2ViewController.swift
+//  StoryboardExample
+//
+//  Created by Gak_ee on 2020/01/31.
+//  Copyright © 2020 Gak_eee. All rights reserved.
+//
+
+import UIKit
+
+class Sub_2ViewController : UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.title  = "StoryBoard Sub_2 VC"
+    }
+}
